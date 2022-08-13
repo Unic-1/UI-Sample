dee0f6135d5cb42a831ff479e62ad34cd331fc22
satyam <satyam123.sr@gmail.com>
2022-08-13 19:38:49 +0530

    - am pm logic added

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -3,9 +3,14 @@ const date = new Date();
 setInterval(() => {
   date.setSeconds(date.getSeconds() + 1);
 
-  document.querySelector("#hour").innerHTML = doubleDigit(date.getHours());
+  const hour = date.getHours();
+  document.querySelector("#hour").innerHTML = doubleDigit(
+    hour > 12 ? hour - 12 : hour
+  );
   document.querySelector("#min").innerHTML = doubleDigit(date.getMinutes());
   document.querySelector("#sec").innerHTML = doubleDigit(date.getSeconds());
+  document.querySelector("#am-pm").innerHTML =
+    date.getHours() >= 12 ? "pm" : "am";
 }, 1000);
 
 function doubleDigit(val) {
